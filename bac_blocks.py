9f457aa0225dbd00481c1d86709fd34050bb7636
mr-bacco <mrbacco04@gmail.com>
2019-09-25 16:19:09 +0100

    tested

--- bac_blocks.py
+++ bac_blocks.py
@@ -9,31 +9,36 @@ import json
 import datetime as date
 
 class Block(): # generation of the class called Block, every block will be an instance of this class
-    def __init__(self, nonce, timestamp, data, prev_hash = ''):
-        self.nonce = nonce
+    def __init__(self, index, timestamp, data, prev_hash = ''):
+        self.index = index
         self.timestamp = str(date.datetime.now())
         self.data = data
         self.prev_hash = prev_hash
-        self.hash = self.hash_calc()
+        self.hash = self.hash_calc() #defining a function to calculate the hash see below
 
-    def hash_calc(self):
+    #function to create the hash from the parameters of the blocks
+    def hash_calc(self): #self means that the function contains nothing
         hash_block=json.dumps( #using the method jason.dumps that allows to serialize the objects in a json format
-            {
-                "nonce":self.nonce, 
+            { # this is the dictionary that is gonna be used to define the parameters of the block
+                "index":self.index, 
                 "timestamp": self.timestamp, 
                 "data":self.data, 
                 "prev_hash": self.prev_hash
             },
-        ).encode()
-        return hashlib.sha256(hash_block).hexdigest() # creation of the hash from the block parameters as defined in the class
-
-    def __str__(self): # returning the string representation of the objects
-        string="nonce: " +str(self.nonce) +"\n"
+        ).encode() #needs to be encoded to be passed to the hash function
+        return hashlib.sha3_256(hash_block).hexdigest() # creation of the hash from the block parameters as defined in the class
+    '''
+    def printhashes(self):
+        print("previous hash :", self.prev_hash)
+        print("current hash :", self.hash)
+    '''
+    def __str__(self): # returning the string representation of the objects so that when I print in the console I can read something
+        string="index: " +str(self.index) +"\n"
         string+="timestamp; " +str(self.timestamp) +"\n"
         string+="data: " +str(self.data) +"\n"
         string+="prev_hash: " +str(self.prev_hash) +"\n"
         string+="hash: " +str(self.hash) +"\n"
         return string
 
-new_block = Block
-print (new_block)
+new_block = Block(100, "01/01/1970", 100)
+
--- bac_chain.py
+++ bac_chain.py
@@ -11,20 +11,20 @@ from bac_blocks import Block
 
 class Bac_Chain(): #these are the actual blocks used for the transactions and the creation of the bac_chain blockchain
     def __init__(self):
-        self.chain=[self.genesis(),]
-
-    def genesis(self):
-        return Block(0, date.datetime.now(), "init_block_data", "")
+        self.chain=[self.genesis(),] # this is the definition of the chain of blocks [which is a list] that has the genesis block as first block as absolute block 0
+                                     
+    def genesis(self): #generation of the initial block of the chain
+        return Block(0, "01/01/1970", "init_block_data", "")
     
-    def get_last_block(self):
-        return self.chain[-1]
+    def last_block(self):
+        return self.chain[-1] # defining the last object of the list as per python syntax
 
-    def add_block(self, newBlock):
-        newBlock.prev_hash=self.get_last_block().hash
-        newBlock.hash=newBlock.hash_calc()
-        self.chain.append(newBlock)
+    def add_block(self, newBlock): #this is the function to add a new block
+        newBlock.prev_hash=self.last_block().hash # the new block has a previous hash value that is the previous block's hash value
+        newBlock.hash=newBlock.hash_calc() # this is calculating the current hash value of the block
+        self.chain.append(newBlock) # this method is appending blocks after using the method append to add object at the end of the list
 
-    def validate(self):
+    def validate(self): #function to check the integrity of the chain
         for i in range(1, len(self.chain)):
             prev_block=self.chain[i-1]
             curr_block=self.chain[i]
@@ -36,31 +36,20 @@ class Bac_Chain(): #these are the actual blocks used for the transactions and th
                 return False
         return True , print("well done mrbacco, your chain is a valid blockchain ")
 
-
-bac = Bac_Chain()
+bac = Bac_Chain() #new object from the Bac_Chain class
 
 bac.add_block(Block(1, date, 90879087))
-bac.add_block(Block(2, date, 23353))
-bac.add_block(Block(3, date, 46773353))
-bac.add_block(Block(4, date, 24553353))
-bac.add_block(Block(5, date, 55453))
-bac.add_block(Block(6, date, 23345453))
-bac.add_block(Block(8, date, 94523353))
-bac.add_block(Block(22, date, 5523353))
-bac.add_block(Block(32, date, 923353))
-bac.add_block(Block(320, date, "payload, lots and lots and lots and lots and lots and lots and lots and lots and lots and lots and lots and lots of data here for fuck's sake"))
-
-
-
-
-
+bac.add_block(Block(13, date, 79087))
+bac.add_block(Block(14, date, 90877))
+bac.add_block(Block(320, date, "payload, lots and lots and lots and lots and lots and lots of data here for fuck's sake"))
+#bac.chain[2].data = 445
+#bac.chain[2].hash = bac.chain[2].hash_calc()
 
 '''
-for x in range (100):
-
-    bac.add_block(Bac_Chain("Bac_Chain " + str(n+1)))
+for i in range (100):
+    bac = bac.add_block(i)
+bac
 '''
-
 for n in bac.chain:
     print(n)
 print(bac.validate())
